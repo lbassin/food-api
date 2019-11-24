0a3417c6fe29e977d7c6e5e40fb89f1da57f3f72
Laurent Bassin <laurent@bassin.info>
2019-11-23 20:09:48 -0500

    Add profiler

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
